585f45d3bdfdea355c136cc58bbd4d6a6d568ee8
Surya Majhi <suryamajhi20@gmail.com>
2024-10-03 17:16:19 +0545

    flag, deflag and board added

--- src/board.rs
+++ src/board.rs
@@ -1,24 +1,142 @@
+use crate::board::CellType::{Flagged, Untouched};
+use rand::{random, Rng};
+use std::fmt::{Display, Formatter};
+
+enum CellType {
+    NoNeighborBombs,
+    Untouched,
+    Flagged,
+}
+
+impl CellType {
+    fn value(&self) -> char {
+        match self {
+            CellType::NoNeighborBombs => ' ',
+            CellType::Untouched => '-',
+            CellType::Flagged => 'F',
+        }
+    }
+}
+
 pub struct Board {
-    players: usize,
+    player_count: usize,
     size_x: usize,
     size_y: usize,
+    dirs: [[i8; 2]; 8],
+    board: Vec<Vec<char>>,
+    bomb: Vec<Vec<bool>>,
 }
 
 impl Board {
     pub fn new(size_x: usize, size_y: usize) -> Self {
+        let bomb_probability = 0.3f32;
+        let mut board = vec![vec!['#'; size_y]; size_x];
+        let mut bomb = vec![vec![false; size_y]; size_x];
+        let mut rng = rand::thread_rng();
+
+        for i in 0..size_x {
+            for j in 0..size_y {
+                board[i][j] = CellType::Untouched.value();
+                if rng.gen_range(0.0..1.0) <= bomb_probability {
+                    bomb[i][j] = true;
+                }
+            }
+        }
+
         Board {
-            players: 0,
+            player_count: 0,
             size_x,
             size_y,
+            dirs: [
+                [-1, -1],
+                [-1, 0],
+                [-1, 1],
+                [0, 1],
+                [0, -1],
+                [1, 0],
+                [1, -1],
+                [1, 1],
+            ],
+            board,
+            bomb,
         }
     }
 
+    /// Assert the correctness of the board and it's representation
+    fn check_rep(&self) {
+        assert_eq!(self.board.len(), self.size_x);
+        assert_eq!(self.board.get(0).unwrap().len(), self.size_y);
+        assert_eq!(self.bomb.len(), self.size_x);
+        assert_eq!(self.bomb.get(0).unwrap().len(), self.size_y);
+    }
+
+    pub fn get_size_x(&self) -> usize {
+        self.size_x
+    }
+
+    pub fn get_size_y(&self) -> usize {
+        self.size_y
+    }
+
     pub fn add_player(&mut self) -> usize {
-        self.players = self.players + 1;
-        self.players
+        self.player_count = self.player_count + 1;
+        self.player_count
     }
 
     pub fn remove_player(&mut self) {
-        self.players = self.players - 1;
+        self.player_count = self.player_count - 1;
+    }
+
+    fn is_valid_coordinate(&self, x: i32, y: i32) -> bool {
+        x >= 0 && x < self.size_x as i32 && y >= 0 && y < self.size_y as i32
+    }
+
+    fn get_adjacent_bombs(&self, x: i32, y: i32) -> usize {
+        let mut bomb_count = 0;
+        for dir in self.dirs {
+            let new_x = x + dir[0] as i32;
+            let new_y = y + dir[1] as i32;
+            if self.is_valid_coordinate(new_x, new_y) && self.bomb[new_x as usize][new_y as usize] {
+                bomb_count += 1;
+            }
+        }
+        bomb_count
+    }
+
+    pub fn flag(&mut self, x: i32, y: i32) -> String {
+        if self.is_valid_coordinate(x, y)
+            && self.board[x as usize][y as usize] == CellType::Untouched.value()
+        {
+            self.board[x as usize][y as usize] = Flagged.value();
+        }
+        format!("{}", self)
+    }
+
+    pub fn deflag(&mut self, x: i32, y: i32) -> String {
+        if self.is_valid_coordinate(x, y) && self.board[x as usize][y as usize] == Flagged.value() {
+            self.board[x as usize][y as usize] = Untouched.value();
+        }
+        format!("{}", self)
+    }
+
+    pub fn dig(&mut self, x: i32, y: i32) -> String {
+        "".into()
+    }
+}
+
+impl Display for Board {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut result = String::new();
+        for i in 0..self.board.len() {
+            for j in 0..self.board.len() {
+                result.push(self.board[i][j]);
+                if j != self.board[i].len() - 1 {
+                    result.push(' ');
+                }
+            }
+            result.push('\n');
+        }
+        result.pop();
+        write!(f, "{}", result)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,23 @@
 pub mod board;
 
 use crate::board::Board;
+use regex::Regex;
 use std::io::{BufRead, BufReader, Write};
 use std::net::TcpStream;
 use std::sync::{Arc, Mutex};
 
-pub fn handle_connection(mut stream: TcpStream, board: Arc<Mutex<Board>>) -> std::io::Result<()> {
+pub fn handle_connection(
+    mut stream: TcpStream,
+    mut board: Arc<Mutex<Board>>,
+) -> std::io::Result<()> {
     let player_count = board.lock().unwrap().add_player();
+    let col = board.lock().unwrap().get_size_y();
+    let row = board.lock().unwrap().get_size_x();
+
     let mut welcome_message = format!(
-        "Welcome to Minesweeper. Player {player_count} including you. Type 'help' for help. \n> ");
+        "Welcome to Minesweeper. Player {player_count} including you. Board: {col} columns by {row} rows. Type 'help' for help. \n> ",
+    );
+
     stream.write_all(welcome_message.as_bytes())?;
     stream.flush().unwrap();
 
@@ -16,7 +25,7 @@ pub fn handle_connection(mut stream: TcpStream, board: Arc<Mutex<Board>>) -> std
     for line in reader.lines() {
         let line = line.unwrap();
 
-        let output = handle_request(&line);
+        let output = handle_request(&line, &mut board);
 
         match output.as_str() {
             "" => break,
@@ -37,11 +46,34 @@ pub fn handle_connection(mut stream: TcpStream, board: Arc<Mutex<Board>>) -> std
     Ok(())
 }
 
-fn handle_request(line: &str) -> String {
-    match line {
-        "bye" => String::new(),
-        "boom" => "BOOM!".to_string(),
-        "help" => "Available commands: help, boom, bye".to_string(),
-        _ => format!("Received: {}", line),
+fn handle_request(line: &str, board: &mut Arc<Mutex<Board>>) -> String {
+    let help = String::from("Commands: \nlook\ndig [X] [Y]\nflag [X] [Y]\ndeflag [X] [Y]\nbye\n");
+    let regex = Regex::new(
+        "(look)|(help)|(bye)|(dig -?\\d+ -?\\d+)|(flag -?\\d+ -?\\d+)|(deflag -?\\d+ -?\\d+)",
+    )
+    .unwrap();
+    match regex.is_match(line) {
+        false => help, // invalid input
+        true => {
+            let mut tokens = line.split_whitespace();
+            let command_token = tokens.next().unwrap();
+            match command_token {
+                "help" => help,
+                "bye" => "".into(),
+                "look" => {
+                    format!("{}", board.lock().unwrap())
+                }
+                &_ => {
+                    let x = tokens.next().unwrap().parse::<i32>().unwrap();
+                    let y = tokens.next().unwrap().parse::<i32>().unwrap();
+                    match command_token {
+                        "dig" => board.lock().unwrap().dig(y, x),
+                        "flag" => board.lock().unwrap().flag(y, x),
+                        "deflag" => board.lock().unwrap().deflag(y, x),
+                        &_ => "".into(),
+                    }
+                }
+            }
+        }
     }
 }
